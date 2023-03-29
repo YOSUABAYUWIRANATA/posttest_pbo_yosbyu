5221c5869002be8b73e849d4d4448515999dc902
YOSUA BAYU WIRANATA <113372842+YOSUABAYUWIRANATA@users.noreply.github.com>
2023-03-29 23:37:27 +0800

    Add files via upload

---
+++ POSTTEST4/pemesanan_hotel/src/yosua/hotel.java
@@ -0,0 +1,45 @@
+package yosua;
+
+public class hotel {
+    private String name;
+    private int harga;
+    private int nomer;
+    
+    public hotel(String name, int harga,int nomer) {
+        this.name = name;
+        this.harga = harga;
+        this.nomer = nomer;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getHarga() {
+        return harga;
+    }
+    public int getnomer(){
+        return nomer;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @param harga the harga to set
+     */
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+
+    /**
+     * @param nomer the nomer to set
+     */
+    public void setNomer(int nomer) {
+        this.nomer = nomer;
+    }
+}
